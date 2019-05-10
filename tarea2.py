d5b9908e8d0383ff1bf5571fb27159ff31299b73
AlexanderPrincipe <principetolentino@hotmail.com>
2019-05-10 14:31:18 -0500

    Avance Sabado

--- tarea2.py
+++ tarea2.py
@@ -15,3 +15,21 @@ print (total)
 rdd23 = rddSinCabecera.filter(lambda fila : int(fila[3]) < 24)
 total23 = rdd23.count() # Action
 print("Total Jugadores 23:{}".format(total23))
+
+
+rddGK = rdd23.filter(lambda fila : fila[21] == "GK")
+totalGK = rddGK.count() # Action
+print("Total Jugadores GK:{}".format(totalGK))
+
+rddGKOrden = rddGK.map(lambda fila : (fila[46], 2))
+
+def contador(value1, value2):
+	return value1 + value2
+
+rddGKOrden1 = rddGKOrden.reduceByKey(contador)
+
+rddGKOrden1.foreach(lambda fila : print(fila))
+
+
+#GKDiving, GKHandling, GKKicking, GKPositioning, GKReflexes
+#46-50
