12e49a1cb16352963c8f6c5f885a2c4574dd9db5
Alexander Principe <principetolentinoa@hotmail.com>
2019-05-13 12:29:27 -0500

    Version de entrega

--- tarea2.py
+++ tarea2.py
@@ -21,67 +21,57 @@ rddMedioCampista = rdd.filter(lambda fila : fila[21] == "CDM" or fila[21] == "CM
 rddDelantero = rdd.filter(lambda fila : fila[21] == "CF" or fila[21] == "LF" or fila[21] == "RF" or fila[21] == "LS" or fila[21] == "RS")
 
 
-#*********************EVALUACION POR POSICION************************#
+# Evaluacion Arquero
 def EvaluacionArquero(fila):
-	# Posicion 
 	return (fila[21], fila[2:4] + [fila[11]] + [sum(map(int, fila[87:88]))])
 
 posicion_rddArquero = rddArquero.map(lambda fila: EvaluacionArquero(fila))
 
+# Evaluacion Defensa
 def EvaluacionDefensa(fila):
 	return (fila[21], fila[2:4] + [fila[11]] + [sum(map(int, fila[70:72] + fila[74:76] + fila[81:82]))])
 
 posicion_rddDefensa = rddDefensa.map(lambda fila: EvaluacionDefensa(fila))
 
+# Evaluacion MedioCampista
 def EvaluacionMedioCampista(fila):
 	return (fila[21], fila[2:4] + [fila[11]] + [sum(map(int, fila[56:57] + fila[60:63] + fila[67:68] + fila[76:77]))])
 
 posicion_rddCC = rddMedioCampista.map(lambda fila: EvaluacionMedioCampista(fila))
 
+# Evaluacion delantero
 def EvaluacionDelantero(fila):
 	return (fila[21], fila[2:4] + [fila[11]] + [sum(map(int, fila[55:69] + fila[76:77]))])
 
 posicion_rddDelantero = rddDelantero.map(lambda fila: EvaluacionDelantero(fila))
 
 # Agrupando por posicion
-grupo_rddArquero = posicion_rddArquero.groupByKey()
-grupo_rddArquero = grupo_rddArquero.mapValues(list)
-
-grupo_rddDefensa = posicion_rddDefensa.groupByKey()
-grupo_rddDefensa = grupo_rddDefensa.mapValues(list)
-
-grupo_rddDelantero = posicion_rddDelantero.groupByKey()
-grupo_rddDelantero = grupo_rddDelantero.mapValues(list)
-
-grupo_rddCC = posicion_rddCC.groupByKey()
-grupo_rddCC = grupo_rddCC.mapValues(list)
+grupo_rddArquero = posicion_rddArquero.groupByKey().mapValues(list)
+grupo_rddDefensa = posicion_rddDefensa.groupByKey().mapValues(list)
+grupo_rddDelantero = posicion_rddDelantero.groupByKey().mapValues(list)
+grupo_rddCC = posicion_rddCC.groupByKey().mapValues(list)
 
 # Ordenando por puntaje
 def Ordenamiento_Puntaje(fila):
     # Retornar maximo 2 filas
-	lista_ordenada = sorted(fila, key = lambda x: x[3], reverse = True) 
+	lista_ordenada = sorted(fila, key = lambda x: x[3], reverse = 1) 
 	return lista_ordenada[0:2]
 
-
-# Ordenar por puntaje
-def Ordenamiento(grupo_posicion):
-	return (grupo_posicion[0], Ordenamiento_Puntaje(grupo_posicion[1]))
-
-orden_rddArquero = grupo_rddArquero.map(lambda grupo: Ordenamiento(grupo))
-orden_rddDefensa = grupo_rddDefensa.map(lambda grupo: Ordenamiento(grupo))
-orden_rddCC = grupo_rddCC.map(lambda grupo: Ordenamiento(grupo))
-orden_rddDelantero = grupo_rddDelantero.map(lambda grupo: Ordenamiento(grupo))
+orden_rddArquero = grupo_rddArquero.map(lambda x: (x[0], Ordenamiento_Puntaje(x[1])))
+orden_rddDefensa = grupo_rddDefensa.map(lambda x: (x[0], Ordenamiento_Puntaje(x[1])))
+orden_rddCC = grupo_rddCC.map(lambda x: (x[0], Ordenamiento_Puntaje(x[1])))
+orden_rddDelantero = grupo_rddDelantero.map(lambda x: (x[0], Ordenamiento_Puntaje(x[1])))
 
 # Mostrar datos del jugador
 def mostrar_fila(fila):
-	print("Nombre:     %s" % fila[0])
-	print("Edad:   %s" % fila[1])
-	print("Valor:   %s" % fila[2])
-	print("Puntaje:  %i" % fila[3])
+	print("Nombre:   {}".format(fila[0]))
+	print("Edad:     {}".format(fila[1]))
+	print("Valor:    {}".format(fila[2]))
+	print("Puntaje:  {}".format(fila[3]))
 
 # Mostrar los 2 mejores por posicion
 def mostrar_grupo(grupo_jugadores):
-	print("\n POSICION : %s" % grupo_jugadores[0])
+	print("\n POSICION : {}".format(grupo_jugadores[0]))
 	lista = grupo_jugadores[1]
 	print("\n 1. Titular")
 	mostrar_fila(lista[0])
@@ -89,7 +79,6 @@ def mostrar_grupo(grupo_jugadores):
 		print("\n 2. Suplente")
 		mostrar_fila(lista[1])
 	print("")
-
 
 orden_rddArquero.foreach(lambda grupo: mostrar_grupo(grupo))
 orden_rddDefensa.foreach(lambda grupo: mostrar_grupo(grupo))
